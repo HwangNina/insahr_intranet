15a68b806ebc5e086da398fbf79ea7fd4a1991f3
HwangNina <hwangninaa@gmail.com>
2020-11-16 15:23:25 +0900

    [ADD] 초기세팅

--- intranet_clone/settings.py
+++ intranet_clone/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import my_settings
-DATABASES = my_settings.DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +75,7 @@ WSGI_APPLICATION = 'intranet_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

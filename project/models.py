8103dbf55b2b897e0a1a17e39df9f5abd0602a0c
김예진 <jin11241124@gmail.com>
2020-11-19 14:19:08 +0900

    Add : project model작업중

--- project/models.py
+++ project/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length = 45)
+    description = models.CharField(max_length = 100)
     is_private = models.BooleanField(default = False)
 
     class Meta :
--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from project.views import 뷰이름
 
 urlpatterns = [
-    path('/', SignUpView.as_view()),
 ]

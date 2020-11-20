f4e2fd3dcfe5f53d390b003c462606174800bc47
HwangNina <hwangninaa@gmail.com>
2020-11-20 18:37:45 +0900

    [ADD] 공지사항 검색기능 구현

--- notice/urls.py
+++ notice/urls.py
@@ -3,6 +3,7 @@ from notice.views import NoticeListView, NoticeDetailView
 
 urlpatterns = [
     path('/list', NoticeListView.as_view()),
+    path('/list/<str:search>', NoticeListView.as_view()),
     path('/detail', NoticeDetailView.as_view()),
     path('/detail/<int:notice_id>', NoticeDetailView.as_view())
 ]

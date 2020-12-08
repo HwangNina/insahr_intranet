b444e68329f7b4691e585a4370fa638d7b0fd973
HwangNina <hwangninaa@gmail.com>
2020-12-08 16:33:51 +0900

    [EDIT] employee view 수정

--- employee/urls.py
+++ employee/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from employee.views import SignUpView, SignInView, EmployeeInfoView, ProfileImageView
+from employee.views import SignUpView, SignInView, EmployeeInfoView, ProfileImageView, EmployeeMainView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
     path('/profile', EmployeeInfoView.as_view()),
-    path('/profile/image', ProfileImageView.as_view())
+    path('/profile/image', ProfileImageView.as_view()),
+    path('/profile/main', EmployeeMainView.as_view())
 ]
